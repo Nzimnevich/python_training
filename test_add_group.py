af55153340bdb75113602fbdd0a926dfb2328fae
Nzimnevich <n>
2023-08-04 15:12:22 +0300

    added a fixture and transform unittest to pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,101 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-from group import Group
 import unittest
 
-
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+from group import Group
+from application import Application
+import pytest
 
 
-    def test_add_group_test_case(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="ssdd", header= "sssss", footer= "sss"))
-        self.open_groups_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group_test_case(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.open_groups_page(wd)
-        self.logout(wd)
-    def logout(self, wd):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def create_group(self, wd, group):
-        # init group creation
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[4]").click()
-        # fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element(By.NAME, "submit").click()
-
-    def open_groups_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, wd, user_name, password):
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(user_name)
-        wd.find_element(By.ID, "LoginForm").click()
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/index.php")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
+def test_add_group_test_case(app):
+    app.login(user_name="admin", password="secret")
+    app.create_group(Group(name="ssdd", header="sssss", footer="sss"))
+    app.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_empty_group_test_case(app):
+    app.login(user_name="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
