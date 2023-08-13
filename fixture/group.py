bcd9c04ffe4ac9a6c0f4abf10ec79b7410a353d0
Nzimnevich <n>
2023-08-13 20:07:17 +0300

    changed func like in the lessons

--- fixture/group.py
+++ fixture/group.py
@@ -5,21 +5,32 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, group):
+    def fill_group_form(self, group):
         wd = self.app.wd
-        self.app.navigation.open_groups_page()
-        # init group creation
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[4]").click()
-        # fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.change_field_value("group_name", group)
+        self.change_field_value("group_header", group)
+        self.change_field_value("group_footer", group)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
         wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text.name is not None:
+            wd.find_element(By.NAME, field_name).click()
+            wd.find_element(By.NAME, field_name).clear()
+            wd.find_element(By.NAME, field_name).send_keys(text.name)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.app.navigation.open_groups_page()
+        # init group creation
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[4]").click()
+        # fill group form
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
         self.app.navigation.open_groups_page()
@@ -37,25 +48,20 @@ class GroupHelper:
         wd.find_element(By.NAME, "delete").click()
         self.app.navigation.open_groups_page()
 
-    def changed_first_group(self, group):
+    def changed_first_group(self, new_group_data):
         wd = self.app.wd
         self.app.navigation.open_groups_page()
         # select first group
-        wd.find_element(By.XPATH, "//*[@class ='group']//input").click()
+        self.select_first_group()
         self.click_edit_btn()
         # fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        self.fill_group_form(new_group_data)
         # submit group creation
         wd.find_element(By.NAME, "update").click()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element(By.XPATH, "//*[@class ='group']//input").click()
 
     def click_edit_btn(self):
         wd = self.app.wd
--- test/test_changed_group.py
+++ test/test_changed_group.py
@@ -1,6 +1,9 @@
 from model.group import Group
 
 
-def test_add_group_test_case(app):
-    app.group.changed_first_group(Group(name="cat", header="meo", footer="meow"))
+def test_changed_name_for_group_test_case(app):
+    app.group.changed_first_group(Group(name="cat"))
 
+
+def test_changed_header_for_group_test_case(app):
+    app.group.changed_first_group(Group(header="meo"))
