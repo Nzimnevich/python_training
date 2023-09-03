5621e0e645d9ed2bad520af3cacc528d5a3110b8
Nzimnevich <n>
2023-09-03 15:32:26 +0300

    added page transition

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,6 +2,7 @@ from model.contact import Contact
 
 
 def test_delete_first_contact_test_case(app):
+    app.navigation.open_home_page()
     if app.contact.count() == 0:
         app.navigation.open_add_contact_page()
         app.contact.fill_add_contact_form(Contact(firstname="group for deleting", bday="11", bmonth="February", aday="22", amonth="February", ayear="2020"))
