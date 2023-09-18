23d71b6812d297caed4d90168fff55c60ffbe736
Nzimnevich <n>
2023-09-18 20:34:09 +0300

    fixed a problem with  index = randrange(len(old_contacts))

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,14 +5,15 @@ from random import randrange
 
 def test_delete_first_contact_test_case(app):
     app.navigation.open_home_page()
-    old_contacts = app.contact.get_contacts()
-    index = randrange(len(old_contacts))
     if app.contact.count() == 0:
         app.contact.fill_add_contact_form(
             Contact(firstname="group for deleting", bday="11", bmonth="February", aday="22", amonth="February",
                     ayear="2020"))
         app.contact.click_enter_btn()
         app.navigation.open_home_page()
+    old_contacts = app.contact.get_contacts()
+    index = randrange(len(old_contacts))
+
     app.contact.delete_contact_by_index(index)
     app.navigation.open_home_page()
     new_contacts = app.contact.get_contacts()
